3841259f3505f07502baf67fd1c0d2379320e74d
Vladimir <xalif80@gmail.com>
2023-05-12 13:26:05 +0200

    Implemented MVC, add StartViewController

---
+++ ColorizedApp/ViewControllers/StartViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StartViewController.swift
+//  ColorizedApp
+//
+//  Created by Vlad on 12.5.23..
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+
+}
